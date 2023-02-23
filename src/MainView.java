701cc8b979bf38406ce6cfd93ed8c6e7779bca1a
Facundo Merighi <merighifacundo@gmail.com>
2023-02-23 13:14:04 -0300

    Removing the url of the repository and the labels

--- src/MainView.java
+++ src/MainView.java
@@ -57,33 +57,17 @@ public class MainView extends javax.swing.JFrame {
         this.args = args;
         initComponents();
         this.getRootPane().setDefaultButton(goButton);
-        Preferences p = Preferences.userNodeForPackage(MainView.class);
-        String username = p.get("username", "");
-        userName.setText(username);
-        String url = p.get("repositoryURL", "http://");
-        repositoryURL.setText(url);
+        
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        repositoryURL = new javax.swing.JTextField();
-        userName = new javax.swing.JTextField();
-        password = new javax.swing.JPasswordField();
         goButton = new javax.swing.JButton();
-        clearCache = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
-        jLabel1.setText("RepositoryURL");
-
-        jLabel2.setText("Username");
-
-        jLabel3.setText("Password");
 
         goButton.setText("GO");
         goButton.addActionListener(new java.awt.event.ActionListener() {
@@ -92,12 +76,6 @@ public class MainView extends javax.swing.JFrame {
             }
         });
 
-        clearCache.setText("Clear Cache");
-        clearCache.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                clearCacheActionPerformed(evt);
-            }
-        });
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -108,41 +86,22 @@ public class MainView extends javax.swing.JFrame {
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
                     .add(layout.createSequentialGroup()
                         .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(jLabel1)
-                            .add(jLabel2)
-                            .add(jLabel3))
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(userName, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 305, Short.MAX_VALUE)
-                            .add(repositoryURL, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 305, Short.MAX_VALUE)
-                            .add(password, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 305, Short.MAX_VALUE)))
                     .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
-                        .add(clearCache)
                         .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 242, Short.MAX_VALUE)
                         .add(goButton)))
-                .addContainerGap())
-        );
+                .addContainerGap()))));
+        
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
                 .addContainerGap()
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(jLabel1)
-                    .add(repositoryURL, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(jLabel2)
-                    .add(userName, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(password, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(jLabel3))
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.UNRELATED)
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(goButton)
-                    .add(clearCache))
-                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
+                    .add(goButton))));
+                
+        
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -158,15 +117,7 @@ private void goButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRS
     Runnable run = new Runnable() {
         public void run() {
             goButton.setEnabled(false);
-            clearCache.setEnabled(false);
-            Preferences p = Preferences.userNodeForPackage(MainView.class);
-            String username = userName.getText();
-            String passwd = String.valueOf(password.getPassword());
-            String url = repositoryURL.getText();
-            p.put("username", username);
-            p.put("repositoryURL", url);
-            //SVNHistory hist = new SVNHistory("realtime_sample");
-            //hist.run(url, username, passwd);
+            
             try {
                 System.out.println(args[0]);
                 CodeSwarmConfig cfg = new CodeSwarmConfig(args[0]);
@@ -176,7 +127,7 @@ private void goButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRS
             } catch (IOException e) {
                 System.err.println("Failed due to exception: " + e.getMessage());
                 goButton.setEnabled(true);
-                clearCache.setEnabled(true);
+            
             }
         }
     };
@@ -231,14 +182,8 @@ private void clearCacheActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FI
     private String[] args;
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton clearCache;
+    
     private javax.swing.JButton goButton;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JPasswordField password;
-    private javax.swing.JTextField repositoryURL;
-    private javax.swing.JTextField userName;
     // End of variables declaration//GEN-END:variables
 
 }
